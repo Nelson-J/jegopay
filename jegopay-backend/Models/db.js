a91f884390be2f5f2690e90f62455a7b8e1ea6df
Ntomb Jack <37754421+Nelson-J@users.noreply.github.com>
2020-09-24 15:19:55 +0100

    created database connection for test

---
+++ jegopay-backend/Models/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+const mysql = require('mysql');
+const dbconfig = require('../Config/db.config');
+
+dbconnection = mysql.createConnection({
+    host : dbconfig.HOST,
+    user : dbconfig.USER,
+    password : dbconfig.PASSWORD,
+    multipleStatements : true
+});
+
+//initialise the database to be used;
+//dbcreate = `CREATE DATABASE IF NOT EXISTS ${dbconfig.DATABASE};`;
+
+dbconnection.connect(function(err, result){
+    if(err) throw err;
+    console.log('Server connected');
+});
