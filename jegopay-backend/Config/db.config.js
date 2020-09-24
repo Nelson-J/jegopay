3ad6e7ef87def7a442e2638c8aa63791e44bdf4d
Ntomb Jack <37754421+Nelson-J@users.noreply.github.com>
2020-09-24 17:08:10 +0100

    added a database schema placeholer

--- jegopay-backend/Config/db.config.js
+++ jegopay-backend/Config/db.config.js
@@ -2,5 +2,10 @@ module.exports = {
     HOST : 'localhost',
     USER : 'root',
     PASSWORD : '',
-    DATABASE : 'jegopay' 
+    DATABASE : 'jegopay',
+    SCHEMA : `CREATE TABLE IF NOT EXISTS entity(
+                id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                name VARCHAR(15) NOT NULL,
+                location VARCHAR(15) NOT NULL,
+                nerd BOOLEAN DEFAULT true);`
 }
--- jegopay-backend/Models/db.js
+++ jegopay-backend/Models/db.js
@@ -12,11 +12,18 @@ dbconnection = mysql.createConnection({
 dbcreate = `CREATE DATABASE IF NOT EXISTS ${dbconfig.DATABASE};
     USE ${dbconfig.DATABASE};`;
 
+//table (database schema) create place holder
+schema = dbconfig.SCHEMA;
+
 dbconnection.connect(function(err, result){
     if(err) throw err;
     //console.log('Server connected');
-    dbconnection.query(dbcreate,function(err,result){
+    dbconnection.query(dbcreate, function(err,result){
         if(err) throw err;
-        console.log('Connected to database');
+        //console.log('Connected to database');
+        dbconnection.query(schema, function(err, result){
+            if(err) throw error;
+            console.log('Table created');
+        });
     });
 });
