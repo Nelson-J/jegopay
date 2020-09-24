3f0dd74bdbd31f3b91bb2fe1d011cb1defe81ced
Ntomb Jack <37754421+Nelson-J@users.noreply.github.com>
2020-09-24 17:37:50 +0100

    added action placeholders

--- jegopay-backend/Routes/entity.routes.js
+++ jegopay-backend/Routes/entity.routes.js
@@ -1,1 +1,11 @@
+const express = require('express');
 const entity = require('../Controllers/entity.controller');
+
+const router = express.Router();
+
+//get all users (from //localhost:port/users)
+router.get('/', entity.findAll);
+
+//other actions to be defined
+
+modules.exports = router;
